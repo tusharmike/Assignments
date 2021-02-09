3d3a21a010561d36b0ca24776bd96bc09aba94cb
tusharmike <44191660+tusharmike@users.noreply.github.com>
2021-02-09 19:34:42 +0530

    Day 1 Assignment

---
+++ Day 1/ArithmeticOperation.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+addition = num1 + num2
+substraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print(f'sum:{addition}, sub:{substraction}, mul:{multiplication}, and div:{division}')
---
+++ Day 1/PowerFunc.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+squaring = num1 ** num2
+print(f'num1 to the power num2: {squaring}')
